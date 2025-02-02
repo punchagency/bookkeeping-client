d67864259ab66ef5acd06f08ab1e57f4ff9b002c
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2025-02-02 11:27:53 +0100

    chore: update statement page

--- app/(protected)/statements/page.tsx
+++ app/(protected)/statements/page.tsx
@@ -1,20 +1,20 @@
-
-"use client"
+"use client";
 import React from "react";
 import { useRouter, usePathname } from "next/navigation";
 
-
 const Statements = () => {
+  const pathname = usePathname();
 
-    const pathname = usePathname();
-
-    return (
-        <div>
-            <h1>Statements works!</h1>
-            <p>Current pathname: {pathname}</p>
-        </div>
-    );  
-}
+  return (
+    <div className="container mx-auto p-6 space-y-6">
+      <div>
+        <h1 className="text-3xl font-bold tracking-tight">Statements</h1>
+        <p className="text-muted-foreground">
+          Here&apos;s a list of all your statements
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Statements;
-    
